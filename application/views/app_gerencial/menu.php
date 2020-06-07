54fdc38b34d111b5370e8262b2003f0c8792fb10
Madison César <madisoncesar@gmail.com>
2020-06-07 12:50:49 -0300

    divide em views a estrutura principal

--- application/views/app_gerencial/menu.php
+++ application/views/app_gerencial/menu.php
@@ -1,1 +1,60 @@
-<?php
+<div class="aside-menu-wrapper flex-column-fluid" id="kt_aside_menu_wrapper">
+    <!--begin::Menu Container-->
+    <div id="kt_aside_menu" class="aside-menu my-4" data-menu-vertical="1" data-menu-scroll="1" data-menu-dropdown-timeout="500">
+        <!--begin::Menu Nav-->
+        <ul class="menu-nav">
+            <li class="menu-item" aria-haspopup="true">
+                <a href="index.html" class="menu-link">
+                    <span class="menu-text">Dashboard</span>
+                </a>
+            </li>
+            <li class="menu-item" aria-haspopup="true">
+                <a href="index.html" class="menu-link">
+                    <span class="menu-text">Clientes</span>
+                </a>
+            </li>
+            <li class="menu-item" aria-haspopup="true">
+                <a href="index.html" class="menu-link">
+                    <span class="menu-text">Clientes a ligar</span>
+                </a>
+            </li>
+            <li class="menu-item" aria-haspopup="true">
+                <a href="index.html" class="menu-link">
+                    <span class="menu-text">Vendedores</span>
+                </a>
+            </li>
+            <li class="menu-item" aria-haspopup="true">
+                <a href="index.html" class="menu-link">
+                    <span class="menu-text">Produtos</span>
+                </a>
+            </li>
+            <li class="menu-item" aria-haspopup="true">
+                <a href="index.html" class="menu-link">
+                    <span class="menu-text">Financeiros</span>
+                </a>
+            </li>
+            <li class="menu-item" aria-haspopup="true">
+                <a href="index.html" class="menu-link">
+                    <span class="menu-text">Boletos</span>
+                </a>
+            </li>
+            <li class="menu-item" aria-haspopup="true">
+                <a href="index.html" class="menu-link">
+                    <span class="menu-text">Cobranças</span>
+                </a>
+            </li>
+            <li class="menu-item" aria-haspopup="true">
+                <a href="index.html" class="menu-link">
+                    <span class="menu-text">Vendas</span>
+                </a>
+            </li>
+            <li class="menu-item" aria-haspopup="true">
+                <a href="index.html" class="menu-link">
+                    <span class="menu-text">Comissões</span>
+                </a>
+            </li>
+        </ul>
+        <!--end::Menu Nav-->
+    </div>
+    <!--end::Menu Container-->
+</div>
--- application/views/app_gerencial/scripts.php
+++ application/views/app_gerencial/scripts.php
@@ -1,1 +1,22 @@
-<?php
+<meta charset="utf-8" />
+<title>Metronic | Header Dark</title>
+<meta name="description" content="Header dark theme example" />
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+<!--begin::Fonts-->
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" />
+<!--end::Fonts-->
+<!--begin::Page Vendors Styles(used by this page)-->
+<link href="<?=base_url()?>public/metronic/plugins/custom/fullcalendar/fullcalendar.bundle.css?v=7.0.3" rel="stylesheet" type="text/css" />
+<!--end::Page Vendors Styles-->
+<!--begin::Global Theme Styles(used by all pages)-->
+<link href="<?=base_url()?>public/metronic/plugins/global/plugins.bundle.css?v=7.0.3" rel="stylesheet" type="text/css" />
+<link href="<?=base_url()?>public/metronic/plugins/custom/prismjs/prismjs.bundle.css?v=7.0.3" rel="stylesheet" type="text/css" />
+<link href="<?=base_url()?>public/metronic/css/style.bundle.css?v=7.0.3" rel="stylesheet" type="text/css" />
+<!--end::Global Theme Styles-->
+<!--begin::Layout Themes(used by all pages)-->
+<link href="<?=base_url()?>public/metronic/css/themes/layout/header/base/dark.css?v=7.0.3" rel="stylesheet" type="text/css" />
+<link href="<?=base_url()?>public/metronic/css/themes/layout/header/menu/dark.css?v=7.0.3" rel="stylesheet" type="text/css" />
+<link href="<?=base_url()?>public/metronic/css/themes/layout/brand/dark.css?v=7.0.3" rel="stylesheet" type="text/css" />
+<link href="<?=base_url()?>public/metronic/css/themes/layout/aside/dark.css?v=7.0.3" rel="stylesheet" type="text/css" />
+<!--end::Layout Themes-->
+<link rel="shortcut icon" href="<?=base_url()?>public/metronic/media/logos/favicon.ico" />
